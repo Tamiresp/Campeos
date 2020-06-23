ef7e436f684e8169baac9a65a5412c4bfe168bec
Tamires Pereira <tamires.p.silva@accenture.com>
2020-06-23 19:26:45 -0300

    first commit

---
+++ Campeoes/WinnersTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WinnersTableViewController.swift
+//  Campeoes
+//
+//  Created by tamires.p.silva on 17/06/20.
+//  Copyright © 2020 tamires. All rights reserved.
+//
+
+import UIKit
+
+class WinnersTableViewController: UITableViewController {
+    
+    var worldCups: [WorldCup] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
